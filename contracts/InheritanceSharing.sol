6bfc7b32315ec057896506ae54d5a89013967be2
FDilara <dilara_97yfm@hotmail.com>
2022-10-09 22:54:14 +0300

    add events, modifier

--- contracts/InheritanceSharing.sol
+++ contracts/InheritanceSharing.sol
@@ -7,6 +7,11 @@ import { SafeMath } from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 contract InheritanceSharing {
     // For use mathematical operations
     using SafeMath for uint;
+
+    //Events
+    event PublishTestament(address _testatorAddress, uint _totalAmount);
+    event ProofDeath(address _testatorAddress, address indexed _heirAddress, uint _proofTime);
+    event ReceiptedInheritance(address _testatorAddress, address indexed _heirAddress, bool _isReceipted);
  
     //Information of a testator
     struct Testator {
@@ -35,6 +40,12 @@ contract InheritanceSharing {
     //There is no data to write to state while deploying
     constructor() {}
 
+    //Control to heir for inheritance
+    modifier checkHeirAddress(address _heirAddress) {
+        require(_heirAddress == msg.sender, "not heir address");
+        _;
+    }
+
     //Address that does not testament a publishs testament
     function publishTestament(address _inheritanceToken, address[] memory _heirs, uint _time, uint _totalAmount) external {
         Testator storage testator = testators[msg.sender];
@@ -48,12 +59,13 @@ contract InheritanceSharing {
         allTestators.push(msg.sender);
         ERC20 inheritanceToken = ERC20(testator.inheritanceToken);
         inheritanceToken.transferFrom(msg.sender, address(this), testator.totalAmount);
+
+        emit PublishTestament(msg.sender, _totalAmount);
     }
 
     //Each heir proof the death of the testator
-    function proofToDeath(address _testatorAddress, address _heirAddress) external {
+    function proofToDeath(address _testatorAddress, address _heirAddress) external checkHeirAddress(_heirAddress) {
         require(!testators[_testatorAddress].isDied, "died");
-        require(_heirAddress == msg.sender, "not heir address");
         require(!proof[_testatorAddress][_heirAddress], "proved");
         proof[_testatorAddress][_heirAddress] = true;
         counter[_testatorAddress] += 1;
@@ -61,17 +73,20 @@ contract InheritanceSharing {
             testators[_testatorAddress].isDied = true;
             testators[_testatorAddress].inheritanceBeginTime = (block.timestamp).add(testators[_testatorAddress].time);
         }
+
+        emit ProofDeath(_testatorAddress, _heirAddress, block.timestamp);
     }
 
     //Heir, receipts his inheritance
-    function receiptToTestament(address _testatorAddress, address _heirAddress) external {
+    function receiptToTestament(address _testatorAddress, address _heirAddress) external checkHeirAddress(_heirAddress) {
         require(testators[_testatorAddress].isDied, "did not die");
         require(block.timestamp >= testators[_testatorAddress].inheritanceBeginTime, "not inheritance time");
-        require(_heirAddress == msg.sender, "not heir address");
         require(!isReceipted[_testatorAddress][_heirAddress], "receipted");
         isReceipted[_testatorAddress][_heirAddress] = true;
         ERC20 inheritanceToken = ERC20(testators[_testatorAddress].inheritanceToken);
         inheritanceToken.transfer(_heirAddress, (testators[_testatorAddress].totalAmount.div(testators[_testatorAddress].numberOfHeirs)));
+    
+        emit ReceiptedInheritance(_testatorAddress, _heirAddress, true);
     }
 
     //Read address of all testators
